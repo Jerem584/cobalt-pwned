601521bd8b1245f121227d0628e85b28d911c0d4
Jerem584 <31523935+Jerem584@users.noreply.github.com>
2022-10-18 03:47:27 +0200

    Add files via upload

---
+++ CrackSource/dllmain.cpp
@@ -0,0 +1,101 @@
+// dllmain.cpp : Définit le point d'entrée de l'application DLL.
+#include "framework.h"
+
+static void __stdcall HookFunction( PVOID* destination, PVOID source )
+{
+    DetourTransactionBegin();
+    DetourUpdateThread( GetCurrentThread() );
+    DetourAttach( destination, source );
+    ULONG res = DetourTransactionCommit();
+}
+
+void RunCrack(LPVOID)
+{
+    auto isKernelLoaded = reinterpret_cast<char( __fastcall* )()>(addr + 0xA5BC0)();
+    printf( "[Crack] Waiting for their shit to show up\n" );
+    printf( "[Crack] Is kernel loaded -> %s\n", isKernelLoaded ? "Yes" : "No" );
+    Sleep( 1000 );
+    // Reading usermode b64 from file
+    std::ifstream t( "usermode" );
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+    auto str = buffer.str();
+    std::string* p = &str;
+    std::string pb = std::string();
+    pb.resize( p->size() );
+
+    if ( !isKernelLoaded ) // Is kernel loaded?
+    {
+        printf( "[Crack] Kernel isn't loaded (%d)\n", isKernelLoaded );
+        // Same for kernel
+        std::ifstream t_( "kernel" );
+        std::stringstream buffer_;
+        buffer_ << t_.rdbuf();
+        auto str_ = buffer_.str();
+        std::string* p_ = &str_;
+        std::string pb_ = std::string();
+        pb_.resize( p_->size() );
+        // decode data
+        typedefs::o_Cobalt_Base64( (__int64)&pb_, p_ );
+        printf( "[Crack] Decoding b64 for kernel done!\nPress F2 to load kernel\n" );
+        while ( !GetAsyncKeyState( VK_F2 ) )
+            Sleep(10); // cpu will thnx you 
+
+        // map driver
+        typedefs::o_KDMapper_Run(&pb_);
+        printf( "[Crack] Loaded kernel !\n" );
+    }
+    printf( "[Crack] Decoding base64 for usermode\n" );
+    typedefs::o_Cobalt_Base64( (__int64) &pb, p );
+    
+    printf( "[Crack] Waiting for WZ\n" );
+    while ( !Utils::FindProcessId( L"ModernWarfare.exe" ) )
+        Sleep( 50 );
+    
+    printf( "[Crack] Press F1 whenever you wanna inject\n" );
+    while ( !GetAsyncKeyState( VK_F1 ) )
+        Sleep( 50 );
+
+    typedefs::o_Cobalt_MapAndRun( Utils::FindProcessId( L"ModernWarfare.exe" ), &pb );
+}
+
+// WHY? 
+/* 
+*
+*   We use that as a way for the software to load our dll before the code execute,
+*   by rebuilding import table with our dll. (CFF EXPLORER)
+*
+*/
+extern "C" __declspec(dllexport) int Lol_ImportMe( int code, WPARAM wParam, LPARAM lParam ) {
+    return CallNextHookEx( NULL, code, wParam, lParam );
+}
+#include <thread>
+void start()
+{
+    printf( "Cracked by Theo.#1337 #StopPastingPls\n\n\n" );
+    HookFunction( reinterpret_cast<PVOID*>(& typedefs::o_Cobalt_StateChange), hooked_fnc::hk_Cobalt_StateChange );
+    HookFunction( reinterpret_cast<PVOID*>(&typedefs::o_Cobalt_Base64), hooked_fnc::hk_Cobalt_Base64 );
+    HookFunction( reinterpret_cast<PVOID*>(&typedefs::o_KDMapper_Run), hooked_fnc::hk_kdmapper );
+    HookFunction( reinterpret_cast<PVOID*>(&typedefs::o_connect), hooked_fnc::hk_connect );
+    CreateThread(0, 0, (LPTHREAD_START_ROUTINE)RunCrack, 0, 0, 0 );
+}
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        AllocConsole();
+        freopen( "CONOUT$", "w", stdout );
+        start();
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ CrackSource/framework.h
@@ -0,0 +1,104 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             
+
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+// Process iteration
+#include <TlHelp32.h>
+
+// Hooking
+#pragma lib(comment, "detours.lib")
+#include <detours.h>
+
+// Hooking Utils
+#include <intrin.h>
+#pragma intrinsic(_ReturnAddress)
+
+
+auto addr = (uintptr_t) GetModuleHandleA( 0 );
+
+namespace Utils { // bad habit but idgaf
+	DWORD FindProcessId( const std::wstring& processName )
+	{
+		PROCESSENTRY32 processInfo;
+		processInfo.dwSize = sizeof( processInfo );
+
+		HANDLE processesSnapshot = CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, NULL );
+		if ( processesSnapshot == INVALID_HANDLE_VALUE ) {
+			return 0;
+		}
+
+		Process32First( processesSnapshot, &processInfo );
+		if ( !processName.compare( processInfo.szExeFile ) )
+		{
+			CloseHandle( processesSnapshot );
+			return processInfo.th32ProcessID;
+		}
+
+		while ( Process32Next( processesSnapshot, &processInfo ) )
+		{
+			if ( !processName.compare( processInfo.szExeFile ) )
+			{
+				CloseHandle( processesSnapshot );
+				return processInfo.th32ProcessID;
+			}
+		}
+
+		CloseHandle( processesSnapshot );
+		return 0;
+	}
+}
+
+namespace typedefs {
+	// Function that will map the driver
+	typedef void( __fastcall* t_KDMapper_Run )(std::string* a1);
+	// Map dll and run it using pid and data
+	typedef void( __fastcall* t_Cobalt_MapAndRun )(unsigned int pid, std::string* b64);
+	// Decode Base64
+	typedef __int64( __fastcall* t_Cobalt_Base64 )(__int64 out, std::string* in_b64);
+	// Called at each state change (imgui message)
+	typedef __int64( __fastcall* t_Cobalt_StateChange )(char*, unsigned int);
+	// a json method in nlohmann lib, idk which one thop
+	typedef bool( __fastcall* t_nlohmann_json )(char* a1);
+
+	auto o_KDMapper_Run = (t_KDMapper_Run) (addr + 0xA5C20);
+	auto o_Cobalt_MapAndRun = (t_Cobalt_MapAndRun) (addr + 0xAB4D0);
+	auto o_Cobalt_Base64 = (t_Cobalt_Base64) (addr + 0x10A630);
+	auto o_Cobalt_StateChange = (t_Cobalt_StateChange) (addr + 0xA2CF0);
+	auto o_nlohmann_json = (t_nlohmann_json) (addr + 0x1A6A0);
+
+	// Functions that use boost asio to connect to their ws (shit)
+	typedef int( __stdcall* t_connect )(std::string* lol);
+	auto o_connect = (t_connect) (addr + 0x011DA20);
+};
+
+namespace hooked_fnc
+{
+	__int64 hk_Cobalt_StateChange( char* a1, unsigned int a2 )
+	{
+		printf( "[Cobalt] State changed to = %s (%llX)\n", a1, PVOID( uintptr_t( _ReturnAddress() ) - addr ) );
+		return typedefs::o_Cobalt_StateChange( a1, a2 );
+	}
+	__int64 hk_Cobalt_Base64( __int64 a1, std::string* a2 ) {
+		printf( "[Crack] a1 = %llX | a2 = %llX (%d) | b = %llX\n", a1, a2, a2->size(), 0 );
+		std::ofstream out( "cobalt_dump.txt" );
+		out << *a2;
+		out.close();
+		return typedefs::o_Cobalt_Base64( a1, a2 );
+	}
+
+	void hk_kdmapper( std::string* a1 )
+	{
+		printf( "[Protection] Prevented their driver from loading\n" );
+		return;
+	}
+	int hk_connect( std::string* )
+	{
+		printf( "[Protection] Blocked connect for security purpose\n" );
+		return 0;
+	}
+};
